a2057a3cfea71b782c51e3b68e2a66bd3671de3d
Aaron Basco <aron42610@hotmail.com>
2021-12-31 13:31:38 +0800

    ADDED sudoku class

---
+++ src/Sudoku.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Sudoku {
+    private int size;
+
+    public Sudoku(){
+        Sudoku sudoku = new Sudoku(9);
+    }
+
+    public Sudoku(int size){
+        this.size = size;
+    }
+}
